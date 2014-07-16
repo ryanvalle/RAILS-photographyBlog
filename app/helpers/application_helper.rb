d2fde9de5588bd1fe1c611c9621037fdda723f8e
Ryan Valle <ryanvalle@me.com>
2014-07-15 23:52:45 -0700

    updated helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,8 @@ module ApplicationHelper
 		"<meta property='og:url' content='http://www.technimg.com'>"+
 		"<meta property='twitter:card' content='summary'>"+
 		"<meta property='twitter:url' content='http://www.technimg.com'>"+
-		"<meta property='twitter:description' content='Technimg combines two words - technology & imaging. My goal with this blog is to share with a community of photographers, from hobbyists to professionals, some of the techniques used in the photos I have been taking. Many of these articles will come from a technical perspective, talking about camera settings, lighting, equipment, thought processes, and post-production (image processing) techniques which led to a given photo.'>"+
+		"<meta property='twitter:image' content='http://www.technimg.com#{image_path("logos/technimg_logo_md.png")}'>"+
+		"<meta property='twitter:description' content='Technimg combines two words - technology & imaging. My goal with this blog is to share with a community of photographers, from hobbyists to professionals, some of the techniques used in the photos I have been taking. Many of these articles will come from a technical perspective, talking about camera settings, lighting, equipment, thought processes, and post-production (image processing) techniques which led to a given photo.'>"
 	end
 
 	private 
