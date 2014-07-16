f1d93eaf1d736a521bd453c8d7bbf3eec56f8bfc
Ryan Valle <ryanvalle@me.com>
2014-07-15 22:30:22 -0700

    updated precompile assets.rb for ckeditor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,11 @@
+Rails.application.config.autoload_paths += %W(/app/models/ckeditor)
+
+Rails.application.config.assets.precompile += Ckeditor.assets
+
 Rails.application.config.assets.precompile += %w( 
 	page.css 
 	admin.css
 	gallery.css
 	article.css
+	ckeditor/*
 )
