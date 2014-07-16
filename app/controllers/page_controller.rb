c40387ccadd7014a2cf2565fe2b76b8765e3eada
Ryan Valle <ryanvalle@me.com>
2014-07-16 00:16:19 -0700

    Updated page controller

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,6 @@
 class PageController < ApplicationController
 	def index
-		@articles = Article.all.limit(8).order("id DESC")
+		@articles = Article.all.limit(8).order("id DESC").where(status: 1)
 	end
 
 	def about
